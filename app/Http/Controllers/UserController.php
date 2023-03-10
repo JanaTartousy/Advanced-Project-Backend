a1d181e0a9d6f2e1bb2def37a31ec19b041dcdfe
Tareq El-Ali <elalitareq@gmail.com>
2023-03-10 16:12:50 +0200

    fixed token expiration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -126,7 +126,7 @@ class UserController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60,
+            'expires_in' => auth()->factory()->getTTL() * 60*24,
             'user' => auth()->user()
         ]);
     }
